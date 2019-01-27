89eddffd3bde639c0572019e3a7cb5ebcee079d0
unknown <chris.a.desilva@gmail.com>
2019-01-27 14:05:26 -0600

    Add functionality for subtraction, mulitiplication, and division. Add operations choice dropdown.

--- src/components/Flashcards.js
+++ src/components/Flashcards.js
@@ -1,48 +1,179 @@
 import React from 'react';
-import '../styles/Flashcards.css'
+import '../styles/Flashcards.css';
+import { Button, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+
+let topNumber = 0;
+let bottomNumber = 0;
+let divisor = 0;
+let dividend = 0;
 
 class Flashcards extends React.Component {
   constructor(props) {
     super(props);
+    
+    this.toggle = this.toggle.bind(this);
     this.state = {
       problemTop: '',
       operation: '',
       bottomNumber: '',
       answer: '',
-      correct: false
+      correct: false,
+      dropdownOpen: false
     }
     this.handleChange = this.handleChange.bind(this);
     this.checkAnswer = this.checkAnswer.bind(this);
   }
 
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
   handleChange(e) {
     e.preventDefault();
-    this.setState({answer: Number(e.target.value)})
+    this.setState({answer: Number(e.target.value)});
+  }
+
+  onKeyDown = (event) => {
+    // 'keypress' event misbehaves on mobile so we track 'Enter' key via 'keydown' event
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      event.stopPropagation();
+      this.checkAnswer();
+    }
   }
 
   generateTop = (min, max) => {
-    let topNumber = Math.floor(Math.random() * (max - min) + min);
-    this.setState({problemTop: topNumber})
+    topNumber = Math.floor(Math.random() * (max - min) + min);
+    this.setState({problemTop: topNumber});
   }
 
   generateBottom = (min, max) => {
-    let bottomNumber = Math.floor(Math.random() * (max - min) + min);
-    this.setState({bottomNumber: bottomNumber})
+    bottomNumber = Math.floor(Math.random() * (max - min) + min);
+    this.setState({bottomNumber: bottomNumber});
+  }
+
+  generateDividend = (min, max) => {
+    dividend = Math.floor(Math.random() * (max - min) + min);
+    if(dividend < 10) {
+      dividend = '0' + dividend
+    }
+    this.setState({problemTop: dividend})
+  }
+
+  generateDivisor = (min, max) => {
+    divisor = Math.floor(Math.random() * (max - min) + min);
+    if(divisor < 10){
+        divisor = "0" + divisor;
+    }
+    if(dividend % divisor){
+        this.generateDivisor(min, max);
+    }
+    this.setState({bottomNumber: divisor})
   }
 
   addition = () => {
     this.generateTop(0, 100);
     this.generateBottom(0, 100);
-    this.setState({operation: '+'})
+    this.setState({operation: '+'});
   }
 
-  checkAnswer() {
+  subtraction = () => {
+    this.generateTop(0, 100);
+    this.generateBottom(0, topNumber);
+    this.setState({operation: '-'});
+  }
+
+  multiplication = () => {
+    this.generateTop(0, 12);
+    this.generateBottom(0, 12);
+    this.setState({operation: 'x'})
+  }
+
+  division = () => {
+    this.generateDividend(1, divisor * 12);
+    this.generateDivisor(1, 12);
+    this.setState({operation: '÷'})
+  }
+
+  getProblem = () => {
+    switch(this.state.operation){
+      default: 
+        this.addition();
+        break;
+      case '+':
+        this.addition();
+        break;
+      case '-':
+        this.subtraction();
+        break;
+      case 'x':
+        this.multiplication();
+        break;
+      case '÷':
+        this.division();
+        break;
+    }
+  }
+
+  markCorrect = () => {
+    this.setState({answer: '', correct: true})
+    this.getProblem();
+  }
+
+  checkAddition = () => {
     if(this.state.problemTop + this.state.bottomNumber === this.state.answer){
-      this.setState({answer: '', correct: true})
-      this.addition();
+      this.markCorrect()
     } else if(this.state.problemTop + this.state.bottomNumber !== this.state.answer) {
       this.setState({answer: '', correct: false})
     }
+  }
+
+  checkSubtraction = () => {
+    if(this.state.problemTop - this.state.bottomNumber === this.state.answer){
+      this.markCorrect()
+    } else if(this.state.problemTop - this.state.bottomNumber !== this.state.answer) {
+      this.setState({answer: '', correct: false})
+    }
+  }
+
+  checkMultiplication = () => {
+    if(this.state.problemTop * this.state.bottomNumber === this.state.answer){
+      this.markCorrect()
+    } else if(this.state.problemTop * this.state.bottomNumber !== this.state.answer) {
+      this.setState({answer: '', correct: false})
+    }
+  }
+
+  checkDivision = () => {
+    if(this.state.problemTop / this.state.bottomNumber === this.state.answer){
+      this.markCorrect()
+    } else if(this.state.problemTop / this.state.bottomNumber !== this.state.answer) {
+      this.setState({answer: '', correct: false})
+    }
+  }
+
+  checkAnswer() {
+    switch(this.state.operation){
+      default: 
+        this.checkAddition();
+        break;
+      case '+':
+        this.checkAddition();
+        break;
+      case '-':
+        this.checkSubtraction();
+        break;
+      case 'x':
+        this.checkMultiplication();
+        break;
+      case '÷': 
+        this.checkDivision();
+        break;
+    }
+    
   }
 
   render(){
@@ -54,11 +185,22 @@ class Flashcards extends React.Component {
           <div id="operation">{this.state.operation}</div>
           <div id="bottomNumber">{this.state.bottomNumber}</div>
         </div>
-        <div id="problemAnswer">
-          <input type="text" id="answerBox" onChange={this.handleChange} value={this.state.answer}/>
-        </div>
-        <button onClick={this.checkAnswer}>Check Answer</button>  
-        <button onClick={this.addition}>Get New Problem</button>
+          <div id="problemAnswer">
+            <input type="text" id="answerBox" onChange={this.handleChange} onKeyDown={this.onKeyDown} value={this.state.answer}/>
+          </div>
+          <Button id="newProblem" type="submit" outline color="primary" onClick={this.getProblem}>New Problem</Button>{' '}
+          <Button id="newProblem" outline color="danger" onClick={this.checkAnswer}>Check Answer</Button>{' '}
+          <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} >
+            <DropdownToggle outline color="warning" caret>
+              Operations
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem onClick={this.addition}>Addition</DropdownItem>
+              <DropdownItem onClick={this.subtraction}>Subtraction</DropdownItem>
+              <DropdownItem onClick={this.multiplication}>Multiplication</DropdownItem>
+              <DropdownItem onClick={this.division}>Division</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
       </div>
     )
   }
